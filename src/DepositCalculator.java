438b2eb655b9300acf3357d6749638c40ca1eb6b
Vladislav Kostylev <kostylev.v@gmail.com>
2023-10-25 12:51:01 +0300

    Комментарии по code style DepositCalculator.java

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
-
+/*
+В целом мне кажется получилось отлично, но парочку улучшений я все таки ниже отметил
+*/
 public class DepositCalculator {
-
+/*у объявления метода нет отступа в 4 пробела, хорошо бы добавить, 
+    чтобы визуально отделить от класса в котором он объявлен и
+    закрывающей фигурной тоже оступа не хватает
+*/
 double calculateComplexPercent(double a, double y, int d) {
        double pay = a * Math.pow((1 + y/12), 12 * d);
           return round(pay, 2);
 }
-
+    
   double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
          return round(doubleAmount+doubleAmount * double_year_rate * deposit_period, 2);
-}
+} //не хватает отступа)
 
     double round(double value, int places) {
        double ScaLe = Math.pow(10, places);
        return Math.round(value*ScaLe)/ScaLe;
 }
-
-    void selectionOfDeposit(){
+    
+    void selectionOfDeposit(){ //хорошо бы здесь иметь пробельчик между закр. круглой и откр. фигурной
+        //для полной красоты я бы сделал отступы в 4 пробела для тела метода относительно его объявления
     int period;
     int action;
     Scanner scanner = new Scanner(System.in);
